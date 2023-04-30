672b1b5af1e704ddb893a5858134a6ae7258210f
migitera.yusuke <migitera.yusuke@donuts.ne.jp>
2023-04-30 16:02:16 +0900

    cra

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Assets</h1>
+      </header>
+      <main>
+        <h2>Ethereum</h2>
+        <p>0</p>
+        <h2>Polygon</h2>
+        <p>0</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
